8c4ed13318f4b78e19b1558351c94f6d89ada254
dopamine-s <motor55005@gmail.com>
2022-02-22 08:07:30 +0300

    Поправляет домашнее задание module2-task1

--- js/main.js
+++ js/main.js
@@ -2,15 +2,13 @@ function getRandomInRange(min, max) {
   return Math.abs(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-getRandomInRange(1, 10);
-// за основу взято: https://myrusakov.ru/js-random-numbers.html
-// возвращается значение по модулю,  передаваемые аргументы могут быть любыми.
+getRandomInRange();
+/** за основу взято: https://myrusakov.ru/js-random-numbers.html
+* возвращается значение по модулю,  передаваемые аргументы могут быть любыми.
+*/
 
-const commentText = document.querySelector('.social__footer-text');
+function checkMaxLength (str, maxLength) {
+  return str.length <= maxLength ? 'true' : 'false';
+}
 
-commentText.oninput = function (maxLength) {
-  return commentText.value.length <= maxLength ? 'true' : 'false';
-};
-
-commentText.oninput(20);
-// за основу взято упражнение тренажера: https://htmlacademy.ru/courses/349/run/11
+checkMaxLength();
