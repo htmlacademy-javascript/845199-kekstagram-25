a566bf2a3ae4ead14f25737e3919b87c24243e6f
dopamine-s <motor55005@gmail.com>
2022-04-02 10:46:28 +0300

    Завершает основную работу над эффектами

--- js/effects.js
+++ js/effects.js
@@ -1,23 +1,8 @@
 const imagePicturePreview = document.querySelector('.img-upload__preview img');
-export {imagePicturePreview};
-
 const effectsList = document.querySelector('.effects__list');
 const effectsItems = effectsList.querySelectorAll('.effects__item');
 const effectLevelValue= document.querySelector('.effect-level__value');
 const effectSlider = document.querySelector('.effect-level__slider');
-
-function onImageChooseEffect () {
-  for (const effectsItem of effectsItems) {
-    const effectsRadio = effectsItem.querySelector('.effects__radio');
-
-    effectsRadio.addEventListener('click', () => {
-      imagePicturePreview.className = '';
-      imagePicturePreview.classList.add(`effects__preview--${effectsRadio.value}`);
-    });
-  }
-}
-
-onImageChooseEffect ();
 
 noUiSlider.create(effectSlider, {
   range: {
@@ -62,6 +47,69 @@ function applyEffect () {
 
 function onEffectSliderUpdate () {
   effectSlider.setAttribute('disabled', true);
+
+  for (const effectsItem of effectsItems) {
+    const effectsRadio = effectsItem.querySelector('.effects__radio');
+
+    effectsRadio.addEventListener('click', () => {
+      imagePicturePreview.className = '';
+      imagePicturePreview.classList.add(`effects__preview--${effectsRadio.value}`);
+
+      if (imagePicturePreview.className === 'effects__preview--chrome' || imagePicturePreview === 'effects__preview--sepia') {
+        effectSlider.removeAttribute('disabled');
+        effectSlider.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 1,
+          },
+          start: 1,
+          step: 0.1,
+        });
+      }
+
+      else if (imagePicturePreview.className === 'effects__preview--marvin') {
+        effectSlider.removeAttribute('disabled');
+        effectSlider.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 100,
+          },
+          start: 100,
+          step: 1,
+        });
+      }
+
+      else if (imagePicturePreview.className === 'effects__preview--phobos') {
+        effectSlider.removeAttribute('disabled');
+        effectSlider.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 3,
+          },
+          start: 3,
+          step: 0.1,
+        });
+      }
+
+      else if (imagePicturePreview.className === 'effects__preview--heat') {
+        effectSlider.removeAttribute('disabled');
+        effectSlider.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 3,
+          },
+          start: 3,
+          step: 0.1,
+        });
+      }
+
+      else if (imagePicturePreview.className === 'effects__preview--none') {
+        effectSlider.setAttribute('disabled', true);
+      }
+    });
+  }
 }
 
 onEffectSliderUpdate ();
+
+export {imagePicturePreview};
